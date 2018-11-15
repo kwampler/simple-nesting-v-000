c5c0ead5b73b8da6583a0205be5bd7cf1cced710
katejsenger <katejsenger@yahoo.com>
2018-11-15 17:24:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ def adding_matz
   programmer_hash
 end
 
-ef changing_alan
+def changing_alan
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -104,12 +104,12 @@ ef changing_alan
           :languages => ["C"]
         }
      }
-  alans_new_info = "GUI"
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     alans_new_info = "GUI"
      
-  programmer_hash[:alan_kay][:known_for] {
-   "GUI"
-  }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
   programmer_hash
+
 end
 
 def adding_to_dennis
