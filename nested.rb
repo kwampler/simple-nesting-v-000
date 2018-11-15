906d988e2d882536cbd5f3e0302abce48062b191
katejsenger <katejsenger@yahoo.com>
2018-11-15 17:14:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -52,14 +52,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
