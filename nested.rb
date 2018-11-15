c02846eb95b6c90fe2ce11b9620e52a583744a5c
katejsenger <katejsenger@yahoo.com>
2018-11-15 17:23:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ ef changing_alan
      }
   alans_new_info = "GUI"
      
-  programmer_hash[:alan_kay][:known_for] ={
+  programmer_hash[:alan_kay][:known_for] => {
    "GUI"
   }
   programmer_hash
